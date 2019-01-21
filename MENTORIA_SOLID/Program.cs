cbe6e6b1bf442c7363f65646917959ce033a9d5c
paulo.machado <paulo.machado@dextra-sw.com>
2019-01-21 15:31:46 -0200

    Initial commit

---
+++ MENTORIA_SOLID/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MENTORIA_SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

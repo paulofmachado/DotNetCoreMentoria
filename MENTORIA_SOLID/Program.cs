c1145cbbca31222b61127a9e35fa1b50a7df1152
paulo.machado <paulo.machado@dextra-sw.com>
2019-01-21 15:38:23 -0200

    initial commit

--- MENTORIA_SOLID/Program.cs
+++ MENTORIA_SOLID/Program.cs
@@ -6,7 +6,23 @@ namespace MENTORIA_SOLID
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nome = "Paulo Machado";
+            var valorNaCarteira = 10000.00D;
+
+            var nomeDoPrimeiroInvestimento = "Poupança";
+            var valorDoPrimeiroInvestimento = 5000.00D;
+            var valorDoImpostoDeRenda = 00.00D;
+            var valodDoIoF = 00.00D;
+            var valorDoICMS = 00.00D;
+            var tempoDeInvestimentoEmDias = 180;
+            var rendimentoMensal = 0.04D;
+
+            var valorTotalDoRendimento = tempoDeInvestimentoEmDias / 30 * rendimentoMensal * valorDoPrimeiroInvestimento / 100;
+            var valorTotal = valorDoPrimeiroInvestimento + valorTotalDoRendimento;
+            Console.WriteLine($"O investimento na {nomeDoPrimeiroInvestimento} no valor de "
+            + $"{valorDoPrimeiroInvestimento} rendeu "
+            + $"{valorTotalDoRendimento}, tendo o {nome} o valor total nesse investimento de "
+            + $"{valorTotal}");
         }
     }
 }
